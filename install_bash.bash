66350a4600a90d22c1c1968e01e476c72bad1cc6
DanielleMustillo <danielle.mustillo@adunaglobal.com>
2025-06-20 15:28:22 -0400

    more additions

--- install_bash.bash
+++ install_bash.bash
@@ -1,3 +1,10 @@
 sudo apt install lua5.4 liblua5.4-dev
 sudo apt install luarocks
 sudo snap install go
+sudo apt install imagemagick
+sudo apt-get install texlive-latex-base
+sudo apt-get install texlive-fonts-recommended
+sudo apt-get install texlive-fonts-extra
+
+npm install -g neovim
+sudo apt install python3-pynvim
