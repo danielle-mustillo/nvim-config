f1c5283f8f5f6a406b79c93cef7acb8cfa3cdbdd
DanielleMustillo <danielle.mustillo@adunaglobal.com>
2025-07-22 17:31:37 -0400

    improve install script

--- install_bash.bash
+++ install_bash.bash
@@ -1,13 +1,13 @@
-sudo apt install lua5.4 liblua5.4-dev
-sudo apt install luarocks
-sudo snap install go
-sudo apt install imagemagick
-sudo apt-get install texlive-latex-base
-sudo apt-get install texlive-fonts-recommended
-sudo apt-get install texlive-fonts-extra
+sudo apt install lua5.4 liblua5.4-dev -y
+sudo apt install luarocks -y
+sudo snap install go --classic
+sudo apt install imagemagick -y
+sudo apt-get install texlive-latex-base -y
+sudo apt-get install texlive-fonts-recommended -y
+sudo apt-get install texlive-fonts-extra -y
 
 npm install -g neovim
-sudo apt install python3-pynvim
+sudo apt install python3-pynvim -y
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
