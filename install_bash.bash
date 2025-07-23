8f9b4ef4279fbea7a2a7adb0af96b1e8eb18d827
DanielleMustillo <danielle.mustillo@adunaglobal.com>
2025-07-23 00:23:18 +0000

    stuff

--- install_bash.bash
+++ install_bash.bash
@@ -11,5 +11,13 @@ sudo snap install nvim --classic
 
 sudo apt install python3-pynvim -y
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+# Fzf
+sudo apt install fzf -y
+echo "source /usr/share/doc/fzf/examples/key-bindings.bash" >> /home/$USER/.bashrc
+
+# LazyGit 
+# https://github.com/jesseduffield/lazygit?tab=readme-ov-file#debian-and-ubuntu
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
