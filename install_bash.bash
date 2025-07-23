4041a9e8a376e44d9511cb8af942d31cec0652b3
DanielleMustillo <danielle.mustillo@adunaglobal.com>
2025-07-23 01:48:49 +0000

    ssh-copy & install updates

--- install_bash.bash
+++ install_bash.bash
@@ -1,6 +1,9 @@
 sudo apt install lua5.4 liblua5.4-dev -y
 sudo apt install luarocks -y
-sudo snap install go --classic
+sudo apt install golang -y  # this is because executing :checkhealth doesn't work with snap (but it probably should....)
+#sudo snap install go --classic
+sudo apt install openjdk-21-jdk-headless # this is because we don't always have javac... again :checkhealth
+
 sudo apt install imagemagick -y
 sudo apt-get install texlive-latex-base -y
 sudo apt-get install texlive-fonts-recommended -y
@@ -9,11 +12,15 @@ sudo apt-get install texlive-fonts-extra -y
 sudo snap install nvim --classic
 #npm install -g neovim
 
-sudo apt install python3-pynvim -y
+sudo apt install python3-pynvim python3-pip -y
 
 # Fzf
 sudo apt install fzf -y
-echo "source /usr/share/doc/fzf/examples/key-bindings.bash" >> /home/$USER/.bashrc
+#echo "source /usr/share/doc/fzf/examples/key-bindings.bash" >> $HOME/.bashrc
+
+sudo apt-get install ripgrep -y 
+sudo apt install fd-find -y
+#ln -s $(which fdfind) ~/.local/bin/fd
 
 # LazyGit 
 # https://github.com/jesseduffield/lazygit?tab=readme-ov-file#debian-and-ubuntu
@@ -21,3 +28,5 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
+
+
