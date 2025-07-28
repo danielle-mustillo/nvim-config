b2ebfa1b4343b4e05c60480d0a227444c358d41a
Danielle Mustillo <dmustillo@gmail.com>
2025-07-27 23:28:28 -0400

    updated for arm setup

--- install_bash.bash
+++ install_bash.bash
@@ -1,32 +1,58 @@
-sudo apt install lua5.4 liblua5.4-dev -y
-sudo apt install luarocks -y
+set -e
+
+sudo apt install luarocks lua5.4 liblua5.4-dev -y
 sudo apt install golang -y  # this is because executing :checkhealth doesn't work with snap (but it probably should....)
 #sudo snap install go --classic
-sudo apt install openjdk-21-jdk-headless # this is because we don't always have javac... again :checkhealth
+sudo apt install openjdk-21-jdk-headless -y # this is because we don't always have javac... again :checkhealth
 
-sudo apt install imagemagick -y
-sudo apt-get install texlive-latex-base -y
-sudo apt-get install texlive-fonts-recommended -y
-sudo apt-get install texlive-fonts-extra -y
+sudo apt install imagemagick texlive-latex-base texlive-fonts-recommended -y
+#sudo apt-get install texlive-fonts-extra -y # This is probably overkill, here for refernce only
 
-sudo snap install nvim --classic
-#npm install -g neovim
-
-sudo apt install python3-pynvim python3-pip -y
+sudo apt install python3-pynvim python3-pip python3-venv -y
 
 # Fzf
 sudo apt install fzf -y
 #echo "source /usr/share/doc/fzf/examples/key-bindings.bash" >> $HOME/.bashrc
 
-sudo apt-get install ripgrep -y 
-sudo apt install fd-find -y
+# Node & NPM
+sudo snap install node --classic
+
+
+# NVIM and Direct Dependencies
+sudo snap install nvim --classic
+#npm install -g neovim
+sudo apt install jq ripgrep fd-find -y
 #ln -s $(which fdfind) ~/.local/bin/fd
+
 
 # LazyGit 
 # https://github.com/jesseduffield/lazygit?tab=readme-ov-file#debian-and-ubuntu
+
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+ARCH=$(uname -m)
+if [[ "$ARCH" == "x86_64" ]]; then
+  ARCHIVE="lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+elif [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
+  ARCHIVE="lazygit_${LAZYGIT_VERSION}_Linux_arm64.tar.gz"
+else
+  echo "Unsupported architecture: $ARCH"
+  exit 1
+fi
+
+URL="https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/${ARCHIVE}"
+
+echo "Downloading $ARCHIVE from $URL"
+curl -Lo lazygit.tar.gz "$URL"
 tar xf lazygit.tar.gz lazygit
-sudo install lazygit -D -t /usr/local/bin/
+sudo install lazygit /usr/local/bin
+rm lazygit
+rm lazygit.tar.gz
+
+echo "lazygit v${LAZYGIT_VERSION} installed successfully!"
+
+touch $HOME/.config/lazygit/config.yml
 
 
+
+# Cleanup
+sudo apt autoremove -y
