8dfbb5c83b3907eaf01e9550babd4ce3f33b7289
DanielleMustillo <danielle.mustillo@adunaglobal.com>
2025-09-04 19:41:08 -0400

    kubernetes plugin + remove coq

---
+++ lua/plugins/kubernetes.lua
@@ -0,0 +1,19 @@
+-- https://www.reddit.com/r/neovim/comments/1d1nrvv/kubectlnvim_navigate_your_cluster_in_a_buffer/
+--
+return {
+    "Ramilito/kubectl.nvim",
+    opts = {},
+    version = "2.*",
+    dependencies = "saghen/blink.download",
+    cmd = { "Kubectl", "Kubectx", "Kubens" },
+    --config = function()
+    --  require("kubectl").setup()
+    --end,
+    keys = {
+      { "<leader>k", '<cmd>lua require("kubectl").toggle()<cr>', desc = "Start kubernetes" },
+      --{ "<C-k>", "<Plug>(kubectl.kill)", ft = "k8s_*" },
+      { "7", "<Plug>(kubectl.view_nodes)", ft = "k8s_*" },
+      { "8", "<Plug>(kubectl.view_overview)", ft = "k8s_*" },
+      { "<C-t>", "<Plug>(kubectl.view_top)", ft = "k8s_*" },
+    },
+}
