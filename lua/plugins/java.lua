f3492ad558d9bff28e661b6c826139af6b371999
DanielleMustillo <danielle.mustillo@adunaglobal.com>
2025-09-08 18:01:23 -0400

    working config for java, need blanket & formatter to work though

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -69,14 +69,14 @@ return {
               },
             },
           },
-          format = {
-            enabled = true,
-            settings = {
-              --url = vim.fn.expand("~/Workspace/eclipse-java-google-style.xml"),
-              url = "file://" .. vim.fn.stdpath("config") .. "/formatters/java.xml",
-              profile = "MyProfile",
-            },
-          },
+          --format = {
+          --  enabled = true,
+          --  settings = {
+          --    --url = vim.fn.expand("~/Workspace/eclipse-java-google-style.xml"),
+          --    url = "file://" .. vim.fn.stdpath("config") .. "/formatters/java.xml",
+          --    profile = "MyProfile",
+          --  },
+          --},
           inlayHints = {
             parameterNames = {
               enabled = "all",
@@ -97,4 +97,32 @@ return {
       return opts
     end,
   },
+  --{
+  --"dsych/blanket.nvim",
+  --opts = {
+  --      -- can use env variables and anything that could be interpreted by expand(), see :h expandcmd()
+  --      -- OPTIONAL
+  --      report_path = vim.fn.getcwd().."/server/target/site/jacoco/jacoco.xml",
+  --      -- refresh gutter every time we enter java file
+  --      -- defauls to empty - no autocmd is created
+  --      filetypes = "java",
+  --      -- for debugging purposes to see whether current file is present inside the report
+  --      -- defaults to false
+  --      silent = false,
+  --      -- can set the signs as well
+  --      signs = {
+  --          priority = 10,
+  --          incomplete_branch = "█",
+  --          uncovered = "█",
+  --          covered = "█",
+  --          sign_group = "Blanket",
+
+  --          -- and the highlights for each sign!
+  --          -- useful for themes where below highlights are similar
+  --          incomplete_branch_color = "WarningMsg",
+  --          covered_color = "Statement",
+  --          uncovered_color = "Error",
+  --      },
+  --  }}
+
 }
