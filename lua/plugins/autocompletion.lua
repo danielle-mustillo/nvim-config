aff50d3f97b92d1ee4537c196c83605801138f9d
DanielleMustillo <danielle.mustillo@adunaglobal.com>
2025-09-08 14:57:49 -0400

    auto completion comment out

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "milanglacier/minuet-ai.nvim",
-    config = function()
-      require("minuet").setup({
-        -- Your configuration options here
-      })
-    end,
-  },
-  { "nvim-lua/plenary.nvim" },
-  -- optional, if you are using virtual-text frontend, blink is not required.
-  { "Saghen/blink.cmp" },
+--  {
+--    "milanglacier/minuet-ai.nvim",
+--    config = function()
+--      require("minuet").setup({
+--        -- Your configuration options here
+--      })
+--    end,
+--  },
+--  { "nvim-lua/plenary.nvim" },
+--  -- optional, if you are using virtual-text frontend, blink is not required.
+--  { "Saghen/blink.cmp" },
   { 
     "hrsh7th/nvim-cmp",
     dependencies = {
@@ -18,7 +18,7 @@ return {
     },
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
-      table.insert(opts.sources, { name = "minuet" })
+      --table.insert(opts.sources, { name = "minuet" })
       --opts.performance.fetching_timeout = 2000
     end,
   },
